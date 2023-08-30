34a6619aa43a6935a2010940f8ee9915fca65493
hieu19491 <mariopuzo19491@gmail.com>
2023-08-30 18:25:45 +0700

    Chuẩn bị bài buổi 2

---
+++ Ly_thuyet_vong_lap.java
@@ -0,0 +1,43 @@
+public class Ly_thuyet_vong_lap {
+    /*
+    // Vòng lặp for
+        System.out.println("Vòng lặp for:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Giá trị của i: " + i);
+        }
+
+        // Vòng lặp while
+        System.out.println("\nVòng lặp while:");
+        int count = 0;
+        while (count < 3) {
+            System.out.println("Số lần lặp: " + count);
+            count++;
+        }
+
+        // Vòng lặp do-while
+        System.out.println("\nVòng lặp do-while:");
+        int x = 0;
+        do {
+            System.out.println("Giá trị của x: " + x);
+            x++;
+        } while (x < 3);
+
+        // Từ khóa break
+        System.out.println("\nTừ khóa break:");
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                break; // Thoát khỏi vòng lặp khi i = 3
+            }
+            System.out.println("Giá trị của i: " + i);
+        }
+
+        // Từ khóa continue
+        System.out.println("\nTừ khóa continue:");
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                continue; // Bỏ qua khi i = 3 và chuyển đến lần lặp tiếp theo
+            }
+            System.out.println("Giá trị của i: " + i);
+        }
+     */
+}
